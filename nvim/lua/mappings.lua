14dc58cf0c5f355937aa25c18ad1ffbcd6d1a0b0
Vo Ho Viet Khoa <vhvkhoa@gmail.com>
2025-12-25 14:45:51 -0600

    remove mapping double semi-colon to colon

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,9 +3,6 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
-
--- Keep `;` free for f/t repeat; use `;;` to open command mode quickly
-map({ "n", "v" }, ";;", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
